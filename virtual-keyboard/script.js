e8ecb19abe9c66df413acb33ddd4efcc42cb65e9
Yauheni Karasiuk <yauheni.karasiuk@gmail.com>
2022-04-27 02:55:10 +0300

    feat: implement sync between hard keys and virtual keys, implement propagation from virtual keys to textarea

---
+++ virtual-keyboard/script.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/extensions
+import Keyboard from './keyboard.js';
+// eslint-disable-next-line import/extensions
+
+const keyboard = new Keyboard();
+keyboard.render();
